858edcf24eac0a17f5e0db910782594bae8126a6
Cara Diann <cara.di42@gmail.com>
2020-04-19 18:12:35 -0700

    :) published

---
+++ Assets/CameraOrbit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    private Transform orbit;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        orbit = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            orbit.transform.rotation = Quaternion.Euler(0,-12 * Time.deltaTime, 0) *  orbit.transform.rotation;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            orbit.transform.rotation = Quaternion.Euler(0,+12 * Time.deltaTime, 0) *  orbit.transform.rotation;
+        }
+    }
+}
